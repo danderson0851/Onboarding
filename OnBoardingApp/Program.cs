2765cf18fbd0622c77178177ccbd011022605ce3
danderson0851 <danderson0851@gmail.com>
2018-01-28 13:43:59 -0600

    Increased communicative console text / redability; added boolean IO

--- OnBoardingApp/Program.cs
+++ OnBoardingApp/Program.cs
@@ -10,12 +10,36 @@ namespace OnBoardingApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            var userName = Console.ReadLine();
-            Console.WriteLine($"Awesome, nice to meet you {userName}");
+            Console.WriteLine("What is your first name?");
+            var firstName = Console.ReadLine();
+            Console.WriteLine($"\nAwesome, hi {firstName}! Your first name has been stored in a variable.");
+            Console.WriteLine("\nNow, what is your last name?");
+            var lastName = Console.ReadLine();
+            Console.WriteLine($"\nSweet! It's nice to meet you, {firstName} {lastName}.");
+            Console.WriteLine("\nYour last name has also been stored in a variable.");
 
+            bool isOwner = false;
+            Console.WriteLine($"\n{firstName}, are you the account owner? | y/n");
+            
 
+            Console.WriteLine("(we are going to give value to a boolean variable named 'isOwner' based on your response)");
 
+            var response = Console.ReadLine();
+
+            if (response == "y")
+            {
+                isOwner = true;
+                Console.WriteLine($"Your response was {response}");
+                Console.WriteLine($"The bool variable isOwner = {isOwner}");
+            }
+            else if (response == "n")
+            {
+                isOwner = false;
+                Console.WriteLine($"Your response was {response}");
+                Console.WriteLine($"The bool variable isOwner = {isOwner}");
+            }
+            
+            Console.WriteLine("\n\n\n This is the end of the app! press any key to exit!");
             Console.ReadLine();
          
 
--- OnBoardingApp/User.cs
+++ OnBoardingApp/User.cs
@@ -9,7 +9,6 @@ namespace OnBoardingApp
     class User
     {
 
-
-
+        
     }
 }
