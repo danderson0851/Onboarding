32d2799018a591a18e5d6c3c8a1f9267a1735e57
danderson0851 <danderson0851@gmail.com>
2018-01-29 10:34:01 -0600

    created new instance of user class from main program

--- OnBoardingApp/Program.cs
+++ OnBoardingApp/Program.cs
@@ -10,16 +10,18 @@ namespace OnBoardingApp
     {
         static void Main(string[] args)
         {
+            var person = new User();
+
             Console.WriteLine("What is your first name?");
-            var firstName = Console.ReadLine();
-            Console.WriteLine($"\nAwesome, hi {firstName}! Your first name has been stored in a variable.");
+            person.FirstName = Console.ReadLine();
+            Console.WriteLine($"\nAwesome, hi {person.FirstName}! Your first name has been stored in a variable.");
             Console.WriteLine("\nNow, what is your last name?");
-            var lastName = Console.ReadLine();
-            Console.WriteLine($"\nSweet! It's nice to meet you, {firstName} {lastName}.");
+            person.LastName = Console.ReadLine();
+            Console.WriteLine($"\nSweet! It's nice to meet you, {person.FirstName} {person.LastName}.");
             Console.WriteLine("\nYour last name has also been stored in a variable.");
 
             bool isOwner = false;
-            Console.WriteLine($"\n{firstName}, are you the account owner? | y/n");
+            Console.WriteLine($"\n{person.FirstName}, are you the account owner? | y/n");
             
 
             Console.WriteLine("(we are going to give value to a boolean variable named 'isOwner' based on your response)");
@@ -38,14 +40,14 @@ namespace OnBoardingApp
                 isOwner = false;
                 Console.WriteLine($"Your response was {response}");
                 Console.WriteLine($"The bool variable isOwner = {isOwner}");
-                Console.WriteLine($"\nIt seems you are not the account owner, Mr or Ms {lastName}, so we're done here. Good day to you");
+                Console.WriteLine($"\nIt seems you are not the account owner, Mr or Ms {person.LastName}, so we're done here. Good day to you");
             }
             else
                 Console.WriteLine("Invalid Response. Get out of here with that nonsense!");
 
             if (isOwner == true)
             {
-                Console.WriteLine($"Okay, Mr/Ms account owner (aka  {firstName} {lastName}, if that is your real name...)");
+                Console.WriteLine($"Okay, Mr/Ms account owner (aka  {person.FirstName} {person.LastName}, if that is your real name...)");
                 Console.WriteLine($"\n What might your pin number be???");
                 Console.WriteLine("** DISCLAIMER ** This is not a request for your actual pin, merely program input");
                 var pin = "";
